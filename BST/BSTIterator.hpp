7d7d4f7a990eb51d4bbd1411c5f7847edd2a9807
Amber Guo <yig152@ucsd.edu>
2019-04-09 20:32:12 -0700

    BSTNode indent, iterator operators

--- BST/BSTIterator.hpp
+++ BST/BSTIterator.hpp
@@ -6,55 +6,54 @@
 using namespace std;
 
 template<typename Data>
-class BSTIterator : public iterator<input_iterator_tag,Data> {
+class BSTIterator : public iterator<input_iterator_tag, Data> {
 
-   private:
+private:
 
-      BSTNode<Data>* curr;
+    BSTNode<Data>* curr;
 
-   public:
+public:
 
-      /** Constructor.  Use the argument to initialize the current BSTNode
-       *  in this BSTIterator.
-       */
-      BSTIterator(BSTNode<Data>* curr): curr(curr) { }
+    /** Constructor.  Use the argument to initialize the current BSTNode
+     *  in this BSTIterator.
+     */
+    BSTIterator(BSTNode<Data>* curr) : curr(curr) { }
 
-      /** Dereference operator. */
-      Data operator*() const {
-         return curr->data;
-      }
+    /** Dereference operator. */
+    Data operator*() const {
+        return curr->data;
+    }
 
-      /** Pre-increment operator. */
-      BSTIterator<Data>& operator++() {
-         curr = curr->successor();
-         return *this;
-      }
+    /** Pre-increment operator. */
+    BSTIterator<Data>& operator++() {
+        curr = curr->successor();
+        return *this;
+    }
 
-      /** Post-increment operator. */
-      BSTIterator<Data> operator++(int) {
-         BSTIterator before = BSTIterator(curr);
-         ++(*this);
-         return before;
-      }
+    /** Post-increment operator. */
+    BSTIterator<Data> operator++(int) {
+        BSTIterator before = BSTIterator(curr);
+        ++(*this);
+        return before;
+    }
 
-      /** Equality test operator.
-       * Notice that other is a reference and not a pointer, thus it
-       * cannot be null. Return true if other is equal to the calling 
-       * object. Two iterators are equal if they point to the same 
-       * BSTNode in the same BST.
-       * TODO */
-      bool operator==(BSTIterator<Data> const & other) const {
+    /** Equality test operator.
+     * Notice that other is a reference and not a pointer, thus it
+     * cannot be null. Return true if other is equal to the calling
+     * object. Two iterators are equal if they point to the same
+     * BSTNode in the same BST.
+     * TODO */
+    bool operator==(BSTIterator<Data> const & other) const {
+        // check if calling obj and other are pointing to the same node
+        return *other == *this;
 
-         return false;
+    }
 
-      }
-
-      /** Inequality test operator. TODO */
-      bool operator!=(BSTIterator<Data> const & other) const {
-
-         return false;
-
-      }
+    /** Inequality test operator. TODO */
+    bool operator!=(BSTIterator<Data> const & other) const {
+        // check if calling obj and other are not pointing to the same node
+        return *other != *this;
+    }
 
 };
 
--- BST/BSTNode.hpp
+++ BST/BSTNode.hpp
@@ -7,69 +7,69 @@ using namespace std;
 template<typename Data>
 class BSTNode {
 
-	public:
+public:
 
-		/** Constructor.  Initialize a BSTNode with the given Data item,
-		 *  no parent, and no children.
-		 */
-		BSTNode(const Data & d) : data(d) {
-			left = right = parent = nullptr;
-		}
+    /** Constructor.  Initialize a BSTNode with the given Data item,
+     *  no parent, and no children.
+     */
+    BSTNode(const Data & d) : data(d) {
+        left = right = parent = nullptr;
+    }
 
-		BSTNode<Data>* left;
-		BSTNode<Data>* right;
-		BSTNode<Data>* parent;
-		Data const data;   // the const Data in this node.
+    BSTNode<Data>* left;
+    BSTNode<Data>* right;
+    BSTNode<Data>* parent;
+    Data const data;   // the const Data in this node.
 
-		/** Return the successor of this BSTNode in a BST, or 0 if none.
-		 *  PRECONDITION: this BSTNode is a node in a BST.
-		 *  POSTCONDITION:  the BST is unchanged.
-		 *  RETURNS: the BSTNode that is the successor of this BSTNode,
-		 *  or 0 if there is none.
-		 */
-		BSTNode<Data>* successor() {
-			// assume that current node is in a BST
-			BSTNode<Data>* curr = this;
-			// right child exists
-			if (curr->right != nullptr) {
-				// find the leftmost child in the right part of tree
-				BSTNode<Data>* temp = curr->right;
-				while (curr->right != nullptr) {
-					temp = temp->left;
-				}
-				return temp;
-			}
+    /** Return the successor of this BSTNode in a BST, or 0 if none.
+     *  PRECONDITION: this BSTNode is a node in a BST.
+     *  POSTCONDITION:  the BST is unchanged.
+     *  RETURNS: the BSTNode that is the successor of this BSTNode,
+     *  or 0 if there is none.
+     */
+    BSTNode<Data>* successor() {
+        // assume that current node is in a BST
+        BSTNode<Data>* curr = this;
+        // right child exists
+        if (curr->right != nullptr) {
+            // find the leftmost child in the right part of tree
+            BSTNode<Data>* temp = curr->right;
+            while (curr->right != nullptr) {
+                temp = temp->left;
+            }
+            return temp;
+        }
 
-			// right child does not exists
-			BSTNode<Data>* pa = curr->parent;
-			// find the node which left child is the parent of curr
-			while (pa != nullptr && (curr == pa->right)) {
-				curr = pa;
-				pa = pa->parent;
-			}
-			if (parent == nullptr) {
-				// the node is the last node of BST, i.e. rightmost
-				return 0;
-			}
-			else {
-				return parent;
-			}
-			
-		}
+        // right child does not exists
+        BSTNode<Data>* pa = curr->parent;
+        // find the node which left child is the parent of curr
+        while (pa != nullptr && (curr == pa->right)) {
+            curr = pa;
+            pa = pa->parent;
+        }
+        if (parent == nullptr) {
+            // the node is the last node of BST, i.e. rightmost
+            return 0;
+        }
+        else {
+            return parent;
+        }
 
-}; 
+    }
+
+};
 
 /** Overload operator<< to print a BSTNode's fields to an ostream. */
 template <typename Data>
 ostream & operator<<(ostream& stm, const BSTNode<Data> & n) {
-	stm << '[';
-	stm << setw(10) << &n;                 // address of the BSTNode
-	stm << "; p:" << setw(10) << n.parent; // address of its parent
-	stm << "; l:" << setw(10) << n.left;   // address of its left child
-	stm << "; r:" << setw(10) << n.right;  // address of its right child
-	stm << "; d:" << n.data;               // its data field
-	stm << ']';
-	return stm;
+    stm << '[';
+    stm << setw(10) << &n;                 // address of the BSTNode
+    stm << "; p:" << setw(10) << n.parent; // address of its parent
+    stm << "; l:" << setw(10) << n.left;   // address of its left child
+    stm << "; r:" << setw(10) << n.right;  // address of its right child
+    stm << "; d:" << n.data;               // its data field
+    stm << ']';
+    return stm;
 }
 
 #endif // BSTNODE_HPP
